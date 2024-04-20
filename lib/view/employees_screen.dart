1d312d406d2ce22bb80f5fa64f12ed8f3b976a79
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-20 22:27:04 +0300

    feat: make mock employee screen

--- lib/view/employees_screen.dart
+++ lib/view/employees_screen.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restobook_mobile_client/view/widgets/employee_list_tile.dart';
+import 'package:restobook_mobile_client/view_model/employee_view_model.dart';
 
-class EmployeesScreen extends StatelessWidget {
+class EmployeesScreen extends StatefulWidget {
   const EmployeesScreen({super.key});
 
   @override
+  State<EmployeesScreen> createState() => _EmployeesScreenState();
+}
+
+class _EmployeesScreenState extends State<EmployeesScreen> {
+  late Future<void> tablesLoading;
+  bool _refreshing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    tablesLoading = Provider.of<EmployeeViewModel>(context, listen: false).load();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Employees list"),
+    return Consumer<EmployeeViewModel>(
+        builder: (context, employeeViewModel, child) {
+          return RefreshIndicator(
+            onRefresh: () async {
+              var promise = context.read<EmployeeViewModel>().load();
+              setState(() {
+                _refreshing = true;
+                tablesLoading = promise;
+              });
+              await promise;
+            },
+            child: FutureBuilder(
+              future: tablesLoading,
+              builder: (ctx, snapshot) {
+                if (!_refreshing && snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Не удалось загрузить столы"),
+                        ElevatedButton(
+                            onPressed: () async {
+                              setState(() {
+                                _refreshing = false;
+                                tablesLoading =
+                                    context.read<EmployeeViewModel>().load();
+                              });
+                            },
+                            child: const Text("Попробовать ещё раз"))
+                      ],
+                    ),
+                  );
+                }
+
+                return ListView.builder(
+                    itemCount: employeeViewModel.employees.length,
+                    itemBuilder: (_, index) => EmployeeListTile(employee: employeeViewModel.employees[index])
+                );
+              },
+            ),
+          );
+        }
     );
   }
 }
---
+++ lib/view/widgets/employee_list_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:restobook_mobile_client/model/employee.dart';
+
+class EmployeeListTile extends StatelessWidget {
+  const EmployeeListTile({super.key, required this.employee});
+
+  final Employee employee;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text("${employee.surname} ${employee.name} ${employee.patronymic}")
+    );
+  }
+}
