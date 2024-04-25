498567eaf4f67fdf8096d0e614f9f3a4e8358163
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-25 17:37:58 +0300

    refact: make scrollable_expanded widget

---
+++ lib/view/shared_widget/scrollable_expanded.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class ScrollableExpanded extends StatelessWidget {
+  const ScrollableExpanded({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints viewportConstraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: viewportConstraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: child,
+                  ),
+                ],
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
