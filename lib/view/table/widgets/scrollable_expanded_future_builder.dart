bf270bf981cdc53f61f37449a25b3878c0adc5fb
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-25 17:37:58 +0300

    refact: make scrollable_expanded widget

--- lib/view/table/widgets/scrollable_expanded_future_builder.dart
+++ lib/view/table/widgets/scrollable_expanded_future_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:restobook_mobile_client/view/shared_widget/scrollable_expanded.dart';
 
 class ScrollableExpandedFutureBuilder extends StatelessWidget {
   const ScrollableExpandedFutureBuilder({super.key, required this.loading, required this.onRefresh, required this.errorLabel, required this.child});
@@ -11,45 +12,30 @@ class ScrollableExpandedFutureBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints viewportConstraints) => SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: viewportConstraints.maxHeight,
-            ),
-            child: IntrinsicHeight(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: FutureBuilder(
-                        future: loading,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return const Center(child: CircularProgressIndicator());
-                          }
-
-                          if (snapshot.hasError) {
-                            return Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  errorLabel,
-                                  ElevatedButton(
-                                      onPressed: onRefresh,
-                                      child: const Text("Попробовать ещё раз"))
-                                ],
-                              ),
-                            );
-                          }
-
-                          return child;
-                        }),
-                  ),
-                ],
-              ),
-            ),
-          )
-      ),
+    return ScrollableExpanded(
+      child: FutureBuilder(
+          future: loading,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+        
+            if (snapshot.hasError) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    errorLabel,
+                    ElevatedButton(
+                        onPressed: onRefresh,
+                        child: const Text("Попробовать ещё раз"))
+                  ],
+                ),
+              );
+            }
+        
+            return child;
+          }),
     );
   }
 
