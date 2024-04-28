419e07666be1efb751a92a18e7aa6857cedb5ce3
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-28 14:50:53 +0300

    feat: make table status color indication

--- lib/view/main_screen/widgets/table_list_tile.dart
+++ lib/view/main_screen/widgets/table_list_tile.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:restobook_mobile_client/model/model.dart';
 import '../../table/screens/table_screen.dart';
 
-
 class TableListTile extends StatelessWidget {
   const TableListTile({super.key, required this.table});
 
@@ -11,17 +10,39 @@ class TableListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var color = switch (table.reservedState) {
+      "FREE" => Colors.greenAccent,
+      "NEAR_RESERVED" => Colors.amberAccent,
+      "RESERVED" => Colors.redAccent,
+      // TODO: Handle this case.
+      String() => throw UnimplementedError(),
+    };
+    if (table.state == "BROKEN") {
+      color = Colors.redAccent;
+    }
+
+    var status = switch (table.reservedState) {
+      "FREE" => "Свободен",
+      "NEAR_RESERVED" => "Бронь в течение часа",
+      "RESERVED" => "Занят",
+    // TODO: Handle this case.
+      String() => throw UnimplementedError(),
+    };
+
     return Material(
-      child: ListTile(
-        title: Text("Стол ${table.number} + ${table.reservedState}"),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TableScreen(table: table))
-          );
-        },
+      child: Card(
+        color: color,
+        child: ListTile(
+          title: Text("Стол ${table.number}"),
+          subtitle: Text(status),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => TableScreen(table: table)));
+          },
+        ),
       ),
     );
   }
-
 }
