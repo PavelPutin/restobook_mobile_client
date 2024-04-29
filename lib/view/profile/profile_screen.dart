2a880a7085cf197a44a2bcfcb5b8bd363454cd51
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-29 13:56:37 +0300

    feat: make basic profile screen

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:restobook_mobile_client/view/shared_widget/icon_button_navigator_pop.dart';
+import 'package:restobook_mobile_client/view_model/application_view_model.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -9,11 +11,41 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const IconButtonNavigatorPop(),
-        title: const Text("Имя сотрудника"),
+        title: Consumer<ApplicationViewModel>(
+          builder: (context, applicationViewModel, child) {
+            return Text(applicationViewModel.authorizedUser!.employee.shortFullName);
+          },
+        ),
       ),
-      body: const Center(
-        child: Text("Profile"),
-      ),
+      body: Consumer<ApplicationViewModel>(
+        builder: (context, applicationViewModel, child) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                      "Имя: ${applicationViewModel.authorizedUser!.employee.name}")
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Фамилия: ${applicationViewModel.authorizedUser!.employee.surname}")
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Отчество: ${applicationViewModel.authorizedUser!.employee.patronymic}")
+                ],
+              ),
+              Row(
+                children: [
+                  Text("Комментарий: ${applicationViewModel.authorizedUser!.employee.comment}")
+                ],
+              )
+            ],
+          );
+        },
+      )
     );
   }
 }
