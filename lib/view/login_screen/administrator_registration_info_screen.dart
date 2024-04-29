400515b8dfc39e1674589339837f19080e0df03e
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-29 15:10:26 +0300

    refact: optimize widgets in registration info screens

--- lib/view/login_screen/administrator_registration_info_screen.dart
+++ lib/view/login_screen/administrator_registration_info_screen.dart
@@ -9,63 +9,60 @@ class AdministratorRegistrationInfoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Как зарегистрироваться"),
       ),
-      body: Center(
+      body: const Center(
         child: Card(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 300),
-            child: const Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Регистрация для администратора"),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "Чтобы получить доступ к приложению, напишите заявку с темой \"Регистрация\" на почту pavelputin2003@yandex.ru")),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "В письме укажите:")),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "- Название ресторана")),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "- Полное имя юридического лица")),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "- ИНН")),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Flexible(
-                        child: Text(
-                            "- Ваше ФИО")),
-                  ],
-                ),
-              ],
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Регистрация для администратора"),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "Чтобы получить доступ к приложению, напишите заявку с темой \"Регистрация\" на почту pavelputin2003@yandex.ru")),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "В письме укажите:")),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "- Название ресторана")),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "- Полное имя юридического лица")),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "- ИНН")),
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                      child: Text(
+                          "- Ваше ФИО")),
+                ],
+              ),
+            ],
           ),
         ),
       )
--- lib/view/login_screen/employee_registration_info_screen.dart
+++ lib/view/login_screen/employee_registration_info_screen.dart
@@ -9,29 +9,26 @@ class EmployeeRegistrationInfoScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Как зарегистрироваться"),
         ),
-        body: Center(
+        body: const Center(
           child: Card(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 300),
-              child: const Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Регистрация для сотрудника"),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                          child: Text(
-                              "Чтобы получить доступ к приложению, обратитесь к администратору вашего ресторана. Он выдаст вам логин и пароль для входа")),
-                    ],
-                  ),
-                ],
-              ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Регистрация для сотрудника"),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                        child: Text(
+                            "Чтобы получить доступ к приложению, обратитесь к администратору вашего ресторана. Он выдаст вам логин и пароль для входа")),
+                  ],
+                ),
+              ],
             ),
           ),
         ));
