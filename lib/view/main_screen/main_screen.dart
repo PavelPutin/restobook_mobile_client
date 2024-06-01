5ce876bfcda1daa89045c052cea9802702e637de
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-06-01 11:19:54 +0300

    fix: change load to loadWithTime method on main screen refresh for employee

--- lib/view/main_screen/main_screen.dart
+++ lib/view/main_screen/main_screen.dart
@@ -12,6 +12,7 @@ import 'package:restobook_mobile_client/view/shared_widget/floating_creation_res
 import 'package:restobook_mobile_client/view/shared_widget/icon_button_push_profile.dart';
 import 'package:restobook_mobile_client/view/shared_widget/scaffold_body_padding.dart';
 import 'package:restobook_mobile_client/view_model/application_view_model.dart';
+import 'package:restobook_mobile_client/view_model/reservation_view_model.dart';
 
 import '../../view_model/table_view_model.dart';
 
@@ -103,7 +104,7 @@ class _MainScreenState extends State<MainScreen> {
                   .authorizedUser!
                   .employee
                   .restaurantId!;
-              var promise = Provider.of<TableViewModel>(context, listen: false)
+              var promise = context.read<TableViewModel>()
                   .loadWithDateTime(restaurantId, DateTime.utc(_date.year, _date.month, _date.day,
                       _time.hour, _time.minute));
               setState(() {
@@ -138,7 +139,9 @@ class _MainScreenState extends State<MainScreen> {
                   .authorizedUser!
                   .employee
                   .restaurantId!;
-              var promise = context.read<TableViewModel>().load(restaurantId);
+              var promise = context.read<TableViewModel>()
+                  .loadWithDateTime(restaurantId, DateTime.utc(_date.year, _date.month, _date.day,
+                  _time.hour, _time.minute));
               setState(() {
                 _tablesLoading = promise;
               });
